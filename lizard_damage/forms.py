aa4e5837414cce0e01827942e8f9d1ec248dead9
Arjan Verkerk <arjan.verkerk@gmail.com>
2012-08-14 12:48:41 +0200

    Add styling and labels to first form.

--- lizard_damage/forms.py
+++ lizard_damage/forms.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*-
+# (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.rst.
+from __future__ import (
+  print_function,
+  unicode_literals,
+  absolute_import,
+  division,
+)
+
 from django import forms
 
 class ContactForm1(forms.Form):
-    subject = forms.CharField(max_length=100)
-    sender = forms.EmailField()
+    name = forms.CharField(
+        max_length=100,
+        label='Hoe wilt u het scenario noemen?',
+    )
+    sender = forms.EmailField(
+        label='Emailadres',
+    )
+
 
 class ContactForm2(forms.Form):
     message = forms.CharField(widget=forms.Textarea)
