ec118cddc997a44b57dbcb3bc91fbdc6576fa378
jack <jack.ha@nelen-schuurmans.nl>
2012-08-30 11:36:08 +0200

    Added experimental email task

--- lizard_damage/admin.py
+++ lizard_damage/admin.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from lizard_damage import models
+from lizard_task.models import SecuredPeriodicTask
 
 
 class DamageEventInline(admin.TabularInline):
@@ -12,7 +13,7 @@ class DamageEventInline(admin.TabularInline):
 class DamageEventAdmin(admin.ModelAdmin):
     list_display = ['__unicode__']
     actions = ['process']
-    
+
     def process(self, request, queryset):
         for damage_event in queryset:
             damage_event.process()
@@ -26,7 +27,7 @@ class DamageEventAdmin(admin.ModelAdmin):
 class DamageScenarioAdmin(admin.ModelAdmin):
     list_display = ['__unicode__']
     inlines = [DamageEventInline]
-    actions = ['process']
+    actions = ['process', 'send_received_email', 'send_finished_email', ]
 
     def process(self, request, queryset):
         for damage_scenario in queryset:
@@ -36,6 +37,32 @@ class DamageScenarioAdmin(admin.ModelAdmin):
             'DamageScenarios calculated.',
         )
     process.short_description = 'Bereken schade voor geselecteerde scenarios'
+
+    def send_received_email(self, request, queryset):
+        """Create a send mail task and put it on the queue."""
+        sent = 0
+        for damage_scenario in queryset:
+            task_name = 'Send received mail for scenario %d' % damage_scenario.id
+            task_kwargs = '{"username": "admin", "taskname": "%s", "damage_scenario_id": "%d"}' % (
+                        task_name, damage_scenario.id)
+            email_task, created = SecuredPeriodicTask.objects.get_or_create(
+                name=task_name,
+                task='lizard_damage.tasks.send_received_email',
+                defaults={'kwargs':task_kwargs},
+                )
+            email_task.kwargs = task_kwargs
+            email_task.save()
+            email_task.send_task(username='admin')
+            sent += 1
+        return self.message_user(
+            request,
+            '%d mail tasks are sent (the mails themselves are sent by the task).' % sent)
+    send_received_email.short_description = 'Zend e-mail dat het scenario is ontvangen'
+
+    def send_finished_email(self, request, queryset):
+        return self.message_user(request, 'TODO')
+    send_finished_email.short_description = 'Zend e-mail dat het scenario is uitgerekend'
+
 
 class UnitAdmin(admin.ModelAdmin):
     list_display = ['name', 'factor']
--- lizard_damage/tasks.py
+++ lizard_damage/tasks.py
@@ -1,6 +1,8 @@
 import logging
 from celery.task import task
 from lizard_task.task import task_logging
+from django.core.mail import send_mail
+from lizard_damage.models import DamageScenario
 
 
 @task
@@ -9,3 +11,19 @@ def damage_task(username=None, taskname=None, loglevel=20):
     logger = logging.getLogger(taskname)
     # Do your thing
     logger.info("Doing my thing")
+
+
+@task
+@task_logging
+def send_received_email(damage_scenario_id, username=None, taskname=None, loglevel=20):
+    logger = logging.getLogger(taskname)
+    # Do your thing
+    logger.info("send_received_mail")
+    damage_scenario = DamageScenario.objects.get(pk=damage_scenario_id)
+    send_mail(
+        'Schademodule: Scenario "%s" ontvangen' % damage_scenario.name,
+        'Schademodule scenario "%s" ontvangen. Als de berekening klaar is krijgt U een nieuw bericht.' % (
+            damage_scenario.name),
+        'no-reply@nelen-schuurmans.nl',
+        [damage_scenario.email], fail_silently=False)
+    logger.info("e-mail has been successfully sent to %s for scenario %s" % (damage_scenario.email, damage_scenario.name))
