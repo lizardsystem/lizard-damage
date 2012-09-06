ced48fdff518d11f652972c7d25093ce932d2fdb
jack <jack.ha@nelen-schuurmans.nl>
2012-09-06 15:07:11 +0200

    Added euros formatting

---
+++ lizard_damage/templatetags/formatting.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+@register.filter()
+def euroformat(value):
+    if value > 100000000:
+        return '&euro; %0.0f miljoen' % (value/1000000.0)
+    if value > 1000000:
+        return '&euro; %0.1f miljoen' % (value/1000000.0)
+    if value > 10000:
+        return '&euro; %0.0f duizend' % (value/1000.0)
+    if value > 1000:
+        return '&euro; %0.1f duizend' % (value/1000.0)
+
+    return '&euro; %0.0f' % (value)
